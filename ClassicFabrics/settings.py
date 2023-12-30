de5f962e1388c78a788b8d27dc408c7d38e60e50
AssadiqTechnology <155197391+AssadiqTechnology@users.noreply.github.com>
2023-12-30 13:58:22 +0000

    Update settings.py
    I added postgresql as the database

--- ClassicFabrics/settings.py
+++ ClassicFabrics/settings.py
@@ -15,7 +15,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 import os
 
-
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
